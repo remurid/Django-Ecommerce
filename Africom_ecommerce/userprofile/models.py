c127849cfa19c315d40565b0d3527f5958df91a3
remurid <rexrid1@gmail.com>
2023-03-21 16:04:53 +0300

    commit

---
+++ Africom_ecommerce/userprofile/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Userprofile(models.Model):
+    user = models.OneToOneField(User, related_name='userprofile', on_delete=models.CASCADE)
+    description = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.user.username 
+
+
+        
